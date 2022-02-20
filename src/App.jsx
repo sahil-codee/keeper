33fb07d255db78a3d97828e0f4d3ef5c394a7d82
Your Name <you@example.com>
2022-02-21 01:27:33 +0530

    Create Keeper app from scrach

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import CreateArea from './Components/CreateArea'
+import Header from './Components/Header'
+import Note from './Components/Note'
+import './index.css'
+
+function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(note) {
+        setNotes(prevNotes => {
+           return [...prevNotes, note]
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+          return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
+  return (
+    <div>
+        <Header />
+        <CreateArea
+        onAdd={addNote}
+        />
+        {notes.map((noteItem, index) => {
+            return (
+            <Note 
+            key={index}
+            id={index}
+            title={noteItem.title} 
+            content={noteItem.content}
+            onDelete={deleteNote} />
+        )})}
+    </div>
+  )
+}
+
+export default App
